0586a1a7d60b52b7e6a8c433149871f7e556416c
lwfe1 <lwfe.3241@gmail.com>
2023-09-09 18:25:05 +0530

    Now the program is executable

--- src/main/xyz/lwfe/onsac/Onsac.java
+++ src/main/xyz/lwfe/onsac/Onsac.java
@@ -1,8 +1,32 @@
 package main.xyz.lwfe.onsac;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class Onsac {
 
-    public static void main(String[] args) {
+    public void launch() {
+        final OnsacSoftware software = new OnsacSoftware(800, 600);
+        final JFrame frame = new JFrame("Onsac");
 
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent event) {
+                super.windowClosing(event);
+                System.exit(0);
+            }
+        });
+
+        frame.add(software);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] argument) {
+        EventQueue.invokeLater(() -> new Onsac().launch());
     }
 }
---
+++ src/main/xyz/lwfe/onsac/OnsacSoftware.java
@@ -0,0 +1,18 @@
+package main.xyz.lwfe.onsac;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class OnsacSoftware extends JPanel {
+
+    public int width;
+    public int height;
+
+    public OnsacSoftware(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.setPreferredSize(new Dimension(width, height));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+}
