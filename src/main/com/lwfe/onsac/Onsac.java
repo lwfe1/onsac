4080e681a3b982ae9bf1ea016db31df1753ed4cf
lwfe1 <lwfe.3241@gmail.com>
2023-09-13 21:23:48 +0530

    Finally figured out how to add Icon 💀

--- src/main/com/lwfe/onsac/Onsac.java
+++ src/main/com/lwfe/onsac/Onsac.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.URL;
 
 public class Onsac {
 
     public void launch() {
         final OnsacSoftware software = new OnsacSoftware(800, 600);
         final JFrame frame = new JFrame("Onsac");
+        final URL filepath = getClass().getClassLoader().getResource("icon.png");
 
         frame.addWindowListener(new WindowAdapter() {
 
@@ -24,11 +26,16 @@ public class Onsac {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-
-        ImageIcon icon = new ImageIcon("icon.png");
-        frame.setIconImage(icon.getImage());
-
         frame.setVisible(true);
+
+
+        if (filepath != null) {
+            ImageIcon icon = new ImageIcon(filepath);
+            frame.setIconImage(icon.getImage());
+            System.out.println("Icon loaded successfully");
+        } else {
+            System.out.println("Icon not found");
+        }
     }
 
     public static void main(String[] argument) {
