2af7d3f0b29896da036104d4e64b2d567bf43534
lwfe1 <lwfe.3241@gmail.com>
2023-09-14 17:19:34 +0530

    Adjustments to positions

--- src/main/java/com/lwfe/onsac/ApplicationWindow.java
+++ src/main/java/com/lwfe/onsac/ApplicationWindow.java
@@ -1,33 +1,41 @@
 package com.lwfe.onsac;
 
-import com.lwfe.onsac.listener.MousePositionTracking;
-
 import javax.swing.*;
+import java.awt.*;
 
 public class ApplicationWindow extends JPanel {
 
     MousePositionTracking tracking = new MousePositionTracking();
 
+    public JLabel text;
     public JButton yesButton;
     public JButton noButton;
 
     public ApplicationWindow() {
         this.setLayout(null);
         this.buttonConfiguration();
+        this.add(text);
         this.add(yesButton);
         this.add(noButton);
         this.addMouseMotionListener(tracking);
     }
 
     public void buttonConfiguration() {
+
+        text = new JLabel();
+        text.setText("Are you gay?");
+        text.setFont(new Font("Arial", Font.BOLD, 28));
+        text.setBounds(320, 10, 250, 50);
+        text.setForeground(Color.WHITE);
+
         yesButton = new JButton();
         yesButton.setText("Yes");
-        yesButton.setBounds(500, 50, 100, 50);
+        yesButton.setBounds(270, 150, 100, 50);
         yesButton.setFocusable(false);
 
         noButton = new JButton();
         noButton.setText("No");
-        noButton.setBounds(500, 400, 100, 50);
+        noButton.setBounds(450, 150, 100, 50);
         noButton.setFocusable(false);
     }
 }
--- src/main/java/com/lwfe/onsac/MousePositionTracking.java
+++ src/main/java/com/lwfe/onsac/MousePositionTracking.java
@@ -1,4 +1,4 @@
-package com.lwfe.onsac.listener;
+package com.lwfe.onsac;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
